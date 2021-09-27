024b1a06bbc203ddfb56a550ce80d6585fcc47ba
Jose A. Molina <josemouribe@gmail.com>
2021-09-27 14:59:24 +0200

    Rendering Hero-detail component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,25 +10,16 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  selectedHero?: Hero;
-
   heroes: Hero[] = [];
 
-  ngOnInit(): void {
-    this.getHeroes();
-  }
+  constructor(private heroService: HeroService) { }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id= ${hero.id}`);
+  ngOnInit() {
+    this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes)
+      .subscribe(heroes => this.heroes = heroes);
   }
-
 }
